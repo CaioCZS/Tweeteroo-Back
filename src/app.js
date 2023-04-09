4c47d604ea9f360550639fafa228ddc3fa2d6b23
Caio <caiozs21@gmail.com>
2023-04-09 19:44:49 -0300

    Req : Get tweets

--- src/app.js
+++ src/app.js
@@ -1,53 +1,63 @@
-import express from "express";
+import express from "express"
 import cors from "cors"
-import { type } from "os";
-const server = express();
-server.use(express.json());
+
+const server = express()
+server.use(express.json())
 server.use(cors())
-let userAtual;
-const usuarios = [
-  {
-    username: "bobesponja",
-    avatar:
-      "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
-  },
-];
-const tweets = [
-  {
-    username: "bobesponja",
-    tweet: "Eu amo hambúrguer de siri!",
-  },
-];
+let userAtual
+const usuarios = []
+const tweets = []
 
-server.post("/sign-up" , (req ,res)=>{
-    const {username ,avatar} = req.body
-    userAtual = { username , avatar}
-if(!username || typeof username !== "string" || !avatar  ||  typeof avatar !== "string" ){
-    return res.sendStatus(400)
-}
+server.post("/sign-up", (req, res) => {
+	const { username, avatar } = req.body
+	userAtual = { username, avatar }
+	if (
+		!username ||
+    typeof username !== "string" ||
+    !avatar ||
+    typeof avatar !== "string"
+	) {
+		return res.sendStatus(400)
+	}
 
-    usuarios.push(userAtual)
-    res.status(201).send("OK")
+	usuarios.push(userAtual)
+	res.status(201).send("OK")
 })
 
-server.post("/tweets" , (req,res)=>{
-    const {tweet} = req.body
-    const {user} = req.headers
-    if(!userAtual){
-       return res.status(401).send("UNAUTHORIZED")
-    }
+server.post("/tweets", (req, res) => {
+	const { tweet } = req.body
+	const { user } = req.headers
+	if (!userAtual) {
+		return res.status(401).send("UNAUTHORIZED")
+	}
 
-    if(!tweet || typeof tweet !== "string"){
-        return res.sendStatus(400)
-    }
-    const newTweet = {username:user , tweet}
-    tweets.push(newTweet) 
+	if (!tweet || typeof tweet !== "string") {
+		return res.sendStatus(400)
+	}
+	const newTweet = { username: user, tweet }
+	tweets.push(newTweet)
 
-    res.status(201).send("OK")
+	res.status(201).send("OK")
 })
 
-server.get("/usu" , (req ,res)=>{
-    res.send(tweets)
+server.get("/tweets", (req, res) => {
+	let tweetsFiltrado = []
+	if(tweets.length < 11){
+		tweets.forEach((t) => {
+			const { avatar } = usuarios.find((u) => u.username === t.username)
+			const newTweet = {...t , avatar }
+			tweetsFiltrado.push(newTweet)
+		})
+		
+	}else{
+		for(let i=tweets.length-1 ; i >= tweets.length - 10 ; i--){
+			const { avatar } = usuarios.find((u) => u.username === tweets[i].username)
+			const newTweet = {...tweets[i] , avatar }
+			tweetsFiltrado.push(newTweet)
+		}
+	}
+	
+	res.send(tweetsFiltrado)
 })
 
-server.listen(5000);
+server.listen(5000)
